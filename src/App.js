7ab3759b8118b5e9f35679fe3592353a982d30b5
Joel <jronceros.byj@gmail.com>
2022-06-25 17:00:18 -0500

    add: using fetch data in useEffect

--- src/App.js
+++ src/App.js
@@ -5,7 +5,25 @@ import Account from "./routes/Account"
 import Home from "./routes/Home"
 import Signin from "./routes/Signin"
 import Signup from "./routes/Signup"
+
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 function App() {
+  const [coins, setCoins] = useState([])
+
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=40&page=1&sparkline=true"
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setCoins(data)
+      })
+  }, [url])
+  console.log(coins)
+
   return (
     <ThemeProvider>
       <Navbar />
