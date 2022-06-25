df3621fc71f67a35fd65f0b587a9470ebc5e6ea8
Joel <jronceros.byj@gmail.com>
2022-06-25 11:32:57 -0500

    add: routes[home,account,signin, signup] from app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import { ThemeProvider } from "./context/ThemeContext"
+import Account from "./routes/Account"
+import Home from "./routes/Home"
+import Signin from "./routes/Signin"
+import Signup from "./routes/Signup"
 function App() {
   return (
     <ThemeProvider>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
     </ThemeProvider>
   )
 }
