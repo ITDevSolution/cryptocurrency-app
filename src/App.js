53ebd380c2730fd05df5bac4cbe7c4584bccae22
Joel <jronceros.byj@gmail.com>
2022-06-25 19:45:57 -0500

    Add: react-sparklines|Components: CoinSearch-CoinItem

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   const [coins, setCoins] = useState([])
 
   const url =
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=40&page=1&sparkline=true"
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=10&page=1&sparkline=true"
 
   useEffect(() => {
     fetch(url)
@@ -22,13 +22,12 @@ function App() {
         setCoins(data)
       })
   }, [url])
-  console.log(coins)
 
   return (
     <ThemeProvider>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home coins={coins} />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/account" element={<Account />} />
--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -1,7 +1,60 @@
-import React from "react"
+import { useState } from "react"
+import CoinItem from "./CoinItem"
 
-const CoinSearch = () => {
-  return <div>CoinSearch</div>
+const CoinSearch = ({ coins }) => {
+  const [searchText, setSearchText] = useState("")
+  // console.log(coins)
+  return (
+    <div>
+      <div>
+        <h1>Search Crypto</h1>
+        {/* Form Search */}
+        <form>
+          <input
+            onChange={(e) => {
+              console.log(e.target.value)
+              setSearchText(e.target.value)
+            }}
+            type="text"
+            placeholder="Search a coin"
+          />
+        </form>
+      </div>
+      {/* Table */}
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>#</th>
+            <th>Coin</th>
+            <th></th>
+            <th>Price</th>
+            <th>24h</th>
+            <th>24 Volume</th>
+            <th>Mkt</th>
+            <th>Last 7 Days</th>
+          </tr>
+        </thead>
+        <tbody>
+          {coins
+            .filter((value) => {
+              if (searchText === "") {
+                return value
+              } else if (
+                value.name
+                  .toLowerCase()
+                  .includes(searchText.toLocaleLowerCase())
+              ) {
+                return value
+              }
+            })
+            .map((coin) => (
+              <CoinItem key={coin.id} coin={coin} />
+            ))}
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 export default CoinSearch
